9e04f213f1b1134cd3ff7f3ae90a8d0e5f064f84
vmaxtacular513 <veronicamaxwell152@gmail.com>
2020-10-19 11:37:02 -0400

    Added javascript toggle functionality to button

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-
-
 // classList - shows/gets all classes
 // contains - checks classList for specific class
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector('.nav-toggler');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function() {
+    console.log(links.classList.contains('random'));
+    links.classList.toggle('show-links');
+});
+
